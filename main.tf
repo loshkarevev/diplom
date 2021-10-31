edd18514f0d96affbf09541b9c27703fedd08c46
loshkarevev <72041312+loshkarevev@users.noreply.github.com>
2021-10-31 14:49:26 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,9 +9,16 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-2"
+  access_key = "AKIA4SYOYXQBNQXUIBMH"
+  secret_key = "/0LwzcBWuG7Mjm6+Cxy7XdnNQ7LRD/Z/AXXfeWIl"
 }
 
 # Create a VPC
 resource "aws_vpc" "example" {
   cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-00399ec92321828f5"
+  instance_type = "t2.micro"
 }
