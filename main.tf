4be02e8c7fe5b91ef1d41acc27ec58344c935afc
loshkarevev <72041312+loshkarevev@users.noreply.github.com>
2021-10-31 14:36:53 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+}
+
+# Configure the AWS Provider
 provider "aws" {
   region = "us-east-2"
+}
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
 }
